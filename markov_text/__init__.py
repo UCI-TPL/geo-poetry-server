5356874df56152dd4d682dd3b60016528bc31048
Jordan Rickman <jrickman@uci.edu>
2016-05-02 16:17:23 -0700

    Avoid generating the empty string, and some setup for unit testing.

--- markov_text/__init__.py
+++ markov_text/__init__.py
@@ -9,16 +9,25 @@ SENTENCE_SEPARATOR = '.'
 WORD_SEPARATOR = ' '
 DB_NAME = 'name'  #value here doesn't matter, only used in command line (markov.py script)
 
-def markov_gen(sentence_list, depth, db_filepath):
-	"""Generator function that generates new sentences from a list of sentences.
+def MarkovGenerator(sentence_list, depth, db_filepath, db = None, rnd = None):
+	"""Generator that generates new sentences from a list of sentences.
 	Arguments:
 		sentence_list		List of strings, each being a single sentence to learn from
 		depth				Depth of analysis at which to build the Markov chain
-		db_filepath			Path to file where sqlite database will be stored"""
-	db = Db(sqlite3.connect(db_filepath), Sql())
-	db.setup(depth)
+		db_filepath			Path to file where sqlite database will be stored
+		db (optional)		Db object (for mocking in unit tests)
+		rnd (optional)		Rnd object (for mocking in unit tests)"""
+	if not db:
+		db = Db(sqlite3.connect(db_filepath), Sql())
+		db.setup(depth)
+	if not rnd:
+		rnd = Rnd()
 
 	parser = Parser(DB_NAME, db, SENTENCE_SEPARATOR).parse_list(sentence_list)
-	generator = Generator(DB_NAME, db, Rnd())
+	generator = Generator(DB_NAME, db, rnd)
 	while True:
-		yield generator.generate(WORD_SEPARATOR)
+		sentence = generator.generate(WORD_SEPARATOR).strip()
+		if len(sentence) == 0:
+			continue # avoid generating the empty string
+		else:
+			yield sentence
