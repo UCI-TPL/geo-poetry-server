00a2058382079511279b74920347f46996c6e64d
Jordan Rickman <jrickman@uci.edu>
2016-05-31 15:34:41 -0700

    Added some logging and error handling to geo-poetry server method.

--- geo_poetry_server.py
+++ geo_poetry_server.py
@@ -4,6 +4,7 @@ from geo_twitter import GeoTweets
 from markov_text import MarkovGenerator
 import os
 from datetime import datetime
+from twython import TwythonAuthError
 
 HEADER_KEY_ACCESS_TOKEN_KEY = "X-TwitterAuthAccessTokenKey"
 HEADER_KEY_ACCESS_TOKEN_SECRET = "X-TwitterAuthAccessTokenSecret"
@@ -11,6 +12,7 @@ CONF_FILE_PATH = "/scratch/twitter.conf"
 MARKOV_DEPTH = 2
 MARKOV_DB_FOLDER = "/scratch/markov_dbs/"
 POEM_LINES_TO_GENERATE = 3
+RESPONSE_KEY_POETRY = 'poetry'
 
 app = Flask(__name__)
 
@@ -20,28 +22,39 @@ def ping():
 
 @app.route("/geo-poetry")
 def get_geo_poetry():
-	# Handle headers (Twitter auth credentials)
 	try:
-		access_token_key = request.headers[HEADER_KEY_ACCESS_TOKEN_KEY]
-		access_token_secret = request.headers[HEADER_KEY_ACCESS_TOKEN_SECRET]
-	except KeyError:
-		abort(403)
-	
-	# TODO Error handling
-	
-	# Fetch Tweets
-	tweets = GeoTweets(consumer_key, consumer_secret, access_token_key, access_token_secret)
+		# Handle headers (Twitter auth credentials)
+		try:
+			access_token_key = request.headers[HEADER_KEY_ACCESS_TOKEN_KEY]
+			access_token_secret = request.headers[HEADER_KEY_ACCESS_TOKEN_SECRET]
+		except KeyError as err:
+			app.logger.error('Received geo-poetry request without header '+str(err.message))
+			abort(403)
 
-	# Generate poetry
-	random_stamp = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(8))
-	filename = datetime.utcnow().isoformat() + '_' + random_stamp
-	markov_db_file = os.path.join(MARKOV_DB_FOLDER, filename)
-	poems = MarkovGenerator(tweets.Tweets(), MARKOV_DEPTH, markov_db_file)
-	poetry = '\n'.join(poems.next() for _ in range(POEM_LINES_TO_GENERATE))
+		# Fetch Tweets
+		try:
+			tweets = GeoTweets(consumer_key, consumer_secret, access_token_key, access_token_secret)
+		except TwythonAuthError as err:
+			app.logger.error('Twitter authentication failed: ' + str(err))
+			abort(403)
 
-	# TODO Get music recommendations
+		# Generate poetry
+		random_stamp = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(8))
+		filename = datetime.utcnow().isoformat() + '_' + random_stamp
+		markov_db_file = os.path.join(MARKOV_DB_FOLDER, filename)
+		poems = MarkovGenerator(tweets.Tweets(), MARKOV_DEPTH, markov_db_file)
+		poetry = '\n'.join(poems.next() for _ in range(POEM_LINES_TO_GENERATE))
 
-	# TODO JSON Response
+		# TODO Get music recommendations
+
+		# Build JSON Response
+		response = {}
+		response[RESPONSE_KEY_POETRY] = poetry
+		return jsonify(response)
+	except Exception as err:
+		app.logger.error('Unexpected Error: ' + str(err))
+		raise err
+		#abort(500) # TODO Abort 500 in production, raise error for debug mode during development
 
 
 if __name__ == "__main__":
