7db75383bf09b66e103f73fd046c963a97a540a5
Jordan Rickman <jrickman@uci.edu>
2016-05-30 22:42:09 -0700

    Beginnings of main geo-peotry server method.

--- geo_poetry_server.py
+++ geo_poetry_server.py
@@ -1,9 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+
+HEADER_KEY_ACCESS_TOKEN_KEY = "X-TwitterAuthAccessTokenKey"
+HEADER_KEY_ACCESS_TOKEN_SECRET = "X-TwitterAuthAccessTokenSecret"
+# TODO Read my Twitter keys (consumer_key, consumer_secret) from conf file
+
+
 app = Flask(__name__)
 
 @app.route("/ping")
 def ping():
     return jsonify({'up': True, 'version': "0.0"})
+
+@app.route("/geo-poetry")
+def get_geo_poetry():
+	# Handle headers (Twitter auth credentials)
+	try:
+		access_token_key = request.headers[HEADER_KEY_ACCESS_TOKEN_KEY]
+		access_token_secret = request.headers[HEADER_KEY_ACCESS_TOKEN_SECRET]
+	except KeyError:
+		abort(403)
 
 if __name__ == "__main__":
 	app.debug = True #TODO Do NOT use debug mode in production
